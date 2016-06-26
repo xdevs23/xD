5662a1e35e82d0c6a48bf8d0492baf7d1ab10c0d
xdevs23 <xdevs23@outlook.com>
2016-06-26 17:54:26 +0200

    xdui: add FlatProgressBar

---
+++ xdui/FlatProgressBar.vb
@@ -0,0 +1,60 @@
+﻿Public Class FlatProgressBar
+
+    Inherits Panel
+
+    Private Shared ReadOnly _
+        NormalSpeed As Integer = CInt(Math.Round(1000 / 136)),
+        InOutSpeed  As Integer = CInt(Math.Round(1000 / 96))
+
+    Private InnerProgressBar As Panel
+    Private WithEvents ProgressBarAdvanceTimer As Timer
+
+    Private ProgressToReach As Integer = 0, ProgressReached As Integer = 0, Progress As Integer
+
+    Public Sub New()
+        BackColor = Color.Transparent
+        
+        InnerProgressBar = New Panel()
+        Controls.Add(InnerProgressBar)
+        InnerProgressBar.Location = New Point(0, 0)
+        InnerProgressBar.Height = Height
+        InnerProgressBar.Anchor = CType(
+            AnchorStyles.Left  + AnchorStyles.Top + 
+            AnchorStyles.Right + AnchorStyles.Bottom,
+            AnchorStyles
+        )
+        InnerProgressBar.BackColor = Color.FromArgb(30, 139, 229)
+    End Sub
+
+    Private Sub SetProgressInternal(Progress As Integer)
+        InnerProgressBar.Width = CInt(Math.Round(Width * (Progress / 100), 0, MidpointRounding.AwayFromZero))
+    End Sub
+
+    Public Sub SetProgress(Progress As Integer)
+        SetProgressInternal(Progress)
+        Me.Progress = Progress
+    End Sub
+
+    Public Sub AdvanceProgress(Progress As Integer)
+        ProgressBarAdvanceTimer = New Timer()
+        ProgressReached = Me.Progress
+        ProgressToReach = Progress
+        Me.Progress = Progress
+        ProgressBarAdvanceTimer.Interval = NormalSpeed
+        ProgressBarAdvanceTimer.Start()
+    End Sub
+
+    Private Sub ProgressBarAdvanceTimer_Tick(sender As Object, e As EventArgs) Handles ProgressBarAdvanceTimer.Tick
+        If ProgressReached < ProgressToReach Then
+            SetProgressInternal(ProgressReached)
+            ProgressReached += 1
+        Else : ProgressBarAdvanceTimer.Stop()
+        End If
+        If ProgressReached < ProgressToReach / 4 Or ProgressReached > ProgressToReach * 0.75 Then
+            ProgressBarAdvanceTimer.Interval = InOutSpeed
+        Else
+            ProgressBarAdvanceTimer.Interval = NormalSpeed
+        End If
+    End Sub
+
+End Class
